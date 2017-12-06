0c4ed67b322d940f80af99d48d2f19bc019e5fab
Vania Dimitrova <vani4ka66@abv.bg>
2017-12-06 12:21:10 +0200

    Create 03. Book-Generator.js

---
+++ 05.  jQuery-DOM-Events/03. Book-Generator.js
@@ -0,0 +1,19 @@
+function createBook(selector,title,author,isbn) {
+
+        let idBook = $('div[id*="book"]').toArray().length + 1;
+        var div  = $('<div>').attr('id',idBook)
+                    .append($('<p>').addClass('title').text(title))
+                    .append($('<p>').addClass('author').text(author))
+                    .append($('<p>').addClass('isbn').text(isbn))
+                    .append($('<button>').text("Select").click(addBorder))
+                    .append($('<button>').text("Deselect").click(removeBorder))
+        $(selector).append(div);
+ 
+        function addBorder() {
+            $(div).css('border','2px solid blue');
+        }
+ 
+        function removeBorder() {
+            $(div).css('border','');
+        }
+    }
