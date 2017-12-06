bd57fca628c0bad702d97a63acc154af8a2804e0
Vania Dimitrova <vani4ka66@abv.bg>
2017-12-06 12:44:34 +0200

    Create 05. Vector-Math.js

---
+++ 07. Functions-and-Objects/05. Vector-Math.js
@@ -0,0 +1,25 @@
+let solution = (function () {
+    
+    return {
+        add: function (vec1, vec2) {
+            return [vec1[0] + vec2[0], vec1[1] + vec2[1]]
+        },
+        multiply: function (vec1, scalar) {
+            return [vec1[0]*scalar, vec1[1]*scalar]
+
+        },
+        length: function (vec1) {
+            return Math.sqrt(Math.pow(vec1[0],2) + Math.pow(vec1[1], 2))
+        },
+        dot: function (vec1, vec2) {
+            return (vec1[0] * vec2[0]) +  (vec1[1] * vec2[1])
+
+        },
+        cross: function (vec1, vec2) {
+            return vec1[0] * vec2[1] - vec1[1] * vec2[0]
+
+        }
+    }
+})();
+
+console.log(solution.dot([2, 3], [2, -1]));
